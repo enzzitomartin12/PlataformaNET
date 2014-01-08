81805d6f003ffd4f0298e5c9caea241aa54b63b1
Victor Valotto <vvalotto@gmail.com>
2014-01-08 15:26:24 -0300

    Creación de la Contenido - Modulo 1

---
+++ Ejercicios/Modulo 1/Demo 1/EjemploWF/EjemploWF/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EjemploWF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Hola " + textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
