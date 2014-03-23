6b254a2ae1ebe5820d210772db9cebfebeb6ba2e
vvalotto <vvalotto@gmail.com>
2014-03-23 11:11:35 -0300

    Prueba

---
+++ Ejercicios Plataforma .NET/Modulo 1/Demo 5/Avion/Avion/Avion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EjemploIntegracion;
+
+namespace IntegracionAvion
+{
+    public class Avion : Vehiculo
+    {
+        public override void DoblarDerecha()
+        {
+            Console.WriteLine("El avión dobla a la derecha");
+        }
+        public override void DoblarIzquierda()
+        {
+            Console.WriteLine("El avión dobla a la Izquiera");
+        }
+        public override void AplicarFrenos()
+        {
+            Console.WriteLine("El avión está frenando");
+        }
+    }
+}
---
+++ Ejercicios Plataforma .NET/Modulo 1/Demo 5/Avion/PruebaAvion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using IntegracionAvion;
+
+namespace IntegracionAvion
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Avion miAvion = new Avion ();
+
+			miAvion.DoblarDerecha ();
+			miAvion.AplicarFrenos ();
+			miAvion.DoblarIzquierda ();
+
+			Console.ReadLine ();
+		}
+	}
+}
